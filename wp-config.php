3b4e86c96c9644c2a4b42a26d30f6e1a49ed377c
Usman Nasir <usman@cyberpersons.com>
2020-06-22 22:07:23 +0500

    update author info

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,8 @@
  * @package WordPress
  */
 
+define( 'WP_DEBUG', true );
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'CCo10ryiHjLuJh' );
