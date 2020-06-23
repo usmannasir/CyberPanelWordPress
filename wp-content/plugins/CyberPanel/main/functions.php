6a2b0ff7f0019dafb55d73f92e1618f5c9c3196f
Usman Nasir <usman@cyberpersons.com>
2020-06-24 00:46:12 +0500

    add menu

--- wp-content/plugins/CyberPanel/main/functions.php
+++ wp-content/plugins/CyberPanel/main/functions.php
@@ -1,13 +1,17 @@
 <?php
 
-add_menu_page(
-    'Cyberpanel', //Page Title
-    'Cyberpanel', //Menu Title
-    'manage_options', //Capability
-    'cyberpanel', //Page slug
-    'cyberpanel_main_html' //Callback to print html
-);
+add_action('admin_menu', 'Main_CyberPanel');
 
+function Main_CyberPanel()
+{
+    add_menu_page(
+        'Cyberpanel', //Page Title
+        'Cyberpanel', //Menu Title
+        'manage_options', //Capability
+        'cyberpanel', //Page slug
+        'cyberpanel_main_html' //Callback to print html
+    );
+}
 function admin_page_html() {
     // check user capabilities
     if ( ! current_user_can( 'manage_options' ) ) {
