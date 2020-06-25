8d6a230e16e6a5c8c51a26b3a99e9a92df66c2fc
Usman Nasir <usman@cyberpersons.com>
2020-06-25 19:08:16 +0500

    finish status

--- wp-content/plugins/CyberPanel/views/connect-server.php
+++ wp-content/plugins/CyberPanel/views/connect-server.php
@@ -2,28 +2,12 @@
 
 
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <h1 class="display-4">Pricing</h1>
-    <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example.
-        It’s built with default Bootstrap components and utilities with little customization.</p>
+    <h1 class="display-4">Connect</h1>
+    <p class="lead">Provide your CyberPanel login details to connect your servers.</p>
 </div>
 
 <div class="container">
     <div class="card-deck mb-3 text-center">
-        <div class="card mb-4 shadow-sm">
-            <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Free</h4>
-            </div>
-            <div class="card-body">
-                <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-                <ul class="list-unstyled mt-3 mb-4">
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Email support</li>
-                    <li>Help center access</li>
-                </ul>
-                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-            </div>
-        </div>
         <div class="card mb-4 shadow-sm">
             <div class="card-header">
                 <h4 class="my-0 font-weight-normal">Pro</h4>
@@ -39,21 +23,6 @@
                     <input type="password" id="password" class="form-control" placeholder="Password" required>
                     <button id="connectServer" class="btn btn-lg btn-primary btn-block" type="button">Connect</button>
                 </form>
-            </div>
-        </div>
-        <div class="card mb-4 shadow-sm">
-            <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Enterprise</h4>
-            </div>
-            <div class="card-body">
-                <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-                <ul class="list-unstyled mt-3 mb-4">
-                    <li>30 users included</li>
-                    <li>15 GB of storage</li>
-                    <li>Phone and email support</li>
-                    <li>Help center access</li>
-                </ul>
-                <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
             </div>
         </div>
     </div>
--- wp-content/plugins/CyberPanel/views/header.php
+++ wp-content/plugins/CyberPanel/views/header.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+<div style="margin-top: 2%" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
     <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="#">Features</a>
@@ -9,7 +9,7 @@
     <a class="btn btn-outline-primary" href="#">Sign up</a>
 </div>
 
-<div class="toast" id="myToast">
+<!--<div class="toast" id="myToast">
     <div class="toast-header">
         <strong class="mr-auto"><i class="fa fa-grav"></i> We miss you!</strong>
         <small>11 mins ago</small>
@@ -18,4 +18,4 @@
     <div id="jobStatusResult" class="toast-body">
         It's been a long time since you visited us. We've something special for you. <a href="#">Click here!</a>
     </div>
-</div>
+</div>-->
