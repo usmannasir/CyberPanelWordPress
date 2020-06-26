e5052268d6524e132a987be24445f038f2559d88
Usman Nasir <usman@cyberpersons.com>
2020-06-26 23:22:10 +0500

    generalise table names

--- wp-content/plugins/CyberPanel/main/CommonUtils.php
+++ wp-content/plugins/CyberPanel/main/CommonUtils.php
@@ -7,7 +7,7 @@ class CommonUtils
 
     function __construct($status, $error)
     {
-        $this->json = json_encode(array('status' => $status, 'result' => $error));
+        $this->json = json_encode(array('status' => 1, 'result' => $error));
     }
 
     function fetchJson(){
