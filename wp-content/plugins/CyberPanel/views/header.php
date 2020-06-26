f4fae78022cebaada0f9cd2ffcb5e61bdbec461c
Usman Nasir <usman@cyberpersons.com>
2020-06-26 23:28:01 +0500

    generalise table names

--- wp-content/plugins/CyberPanel/views/header.php
+++ wp-content/plugins/CyberPanel/views/header.php
@@ -11,12 +11,18 @@
     <a class="btn btn-outline-primary" href="<?php echo admin_url( "admin.php?page=cyberpanel-manage" ); ?>">Manage Servers</a>
 </div>
 
-<div class="toast" id="myToast">
-    <div class="toast-header">
-        <strong class="mr-auto"><i class="fa fa-grav"></i> We miss you!</strong>
-        <small>11 mins ago</small>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
-    </div>
-    <div id="jobStatusResult" class="toast-body">
+<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+    <div class="toast" style="position: absolute; top: 0; right: 0;">
+        <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div id="jobStatusResult" class="toast-body">
+            Hello, world! This is a toast message.
+        </div>
     </div>
 </div>
