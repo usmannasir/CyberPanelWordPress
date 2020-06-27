5109ffa0a84042d0b78e0c96849b7084ea0c8972
Usman Nasir <usman@cyberpersons.com>
2020-06-28 00:36:30 +0500

    load resources selectively

--- wp-content/plugins/CyberPanel/views/header.php
+++ wp-content/plugins/CyberPanel/views/header.php
@@ -1,7 +1,7 @@
 <div style="margin-top: 2%; margin-right: 2%"
      class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <img src="https://cyberpanel.net/wp-content/uploads/2018/07/logo-e1532873145641.png"> <h5
-            class="my-0 mr-md-auto font-weight-normal">CyberPanel</h5>
+    <img src="https://cyberpanel.net/wp-content/uploads/2018/07/logo-e1532873145641.png"> <h4
+            class="my-0 mr-md-auto font-weight-normal"> CyberPanel</h4>
     <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="<?php echo admin_url( "admin.php?page=cyberpanel" ); ?>">Add Servers</a>
         <!--<a class="p-2 text-dark" href="#">Enterprise</a>
