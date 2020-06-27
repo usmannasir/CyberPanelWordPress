a54e3d6826f437ea80eaa596d8906cfec9c0e01a
Usman Nasir <usman@cyberpersons.com>
2020-06-28 00:53:47 +0500

    load resources selectively

--- wp-content/plugins/CyberPanel/views/connect-server.php
+++ wp-content/plugins/CyberPanel/views/connect-server.php
@@ -1,19 +1,28 @@
 <?php include("header.php"); ?>
 
 <div class="container-fluid WPCPContainer">
-    <div class="card-header">
-        <h4 class="my-0 font-weight-normal">Connect</h4>
-    </div>
-    <div class="card-body">
-        <form class="form-signin">
-            <h1 class="h3 mb-3 font-weight-normal">Connect Servers</h1>
-            <label for="hostname" class="sr-only">Hostname</label>
-            <input type="text" id="hostname" class="form-control" placeholder="Hostname" required>
-            <label for="inputEmail" class="sr-only">Username</label>
-            <input type="text" id="username" class="form-control" placeholder="Username" required>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="password" class="form-control" placeholder="Password" required>
-            <button id="connectServer" class="btn btn-lg btn-primary btn-block" type="button">Connect</button>
-        </form>
+    <div class="card">
+        <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Connect</h4>
+        </div>
+        <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                        else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
 </div>
