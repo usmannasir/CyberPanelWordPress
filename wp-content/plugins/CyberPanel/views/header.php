5feaf81373cb200e3e59715bb93aaa033039dbba
Usman Nasir <usman@cyberpersons.com>
2020-06-28 23:05:46 +0500

    improve status function

--- wp-content/plugins/CyberPanel/views/header.php
+++ wp-content/plugins/CyberPanel/views/header.php
@@ -19,15 +19,19 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-
+                <h5 class="modal-title">Jobs Pending/Completed
+                    <div id="WPCPSpinnerModal" class="spinner-border text-info" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div id="jobStatusResult" class="modal-body">
                 <p>Modal body text goes here.</p>
             </div>
             <div class="modal-footer">
-                <div id="WPCPSpinnerModal" class="spinner-border text-info" role="status">
-                    <span class="sr-only">Loading...</span>
-                </div>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button id="viewJobsModal" type="button" class="btn btn-primary">Refresh Status</button>
             </div>
