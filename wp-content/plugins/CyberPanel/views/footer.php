b1f698cccff3f1ce9866feb8090ff353bd3d9a17
Usman Nasir <usman@cyberpersons.com>
2020-06-29 01:55:58 +0500

    hetzner menu

--- wp-content/plugins/CyberPanel/views/footer.php
+++ wp-content/plugins/CyberPanel/views/footer.php
@@ -1,3 +1,16 @@
-<?php
-
-echo 'hello world';
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm WPCPHeader">
+    <img src="https://cyberpanel.net/wp-content/uploads/2018/07/logo-e1532873145641.png"> <h4
+        class="my-0 mr-md-auto font-weight-normal"><span style="margin-left: 5%">CyberPanel</span></h4>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <div id="WPCPSpinner" class="spinner-border text-info" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <a class="p-2 text-dark" href="<?php echo admin_url("admin.php?page=cyberpanel"); ?>">Add Servers</a>
+        <!--<a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>-->
+    </nav>
+    <a class="btn btn-outline-primary" href="<?php echo admin_url("admin.php?page=cyberpanel-manage"); ?>">Manage
+        Servers</a>
+    <a style="margin-left: 1%" id="viewJobs" class="btn btn-outline-info" href="#">View Jobs</a>
+</div>
