d567a148e3ac15a3ae7e8f750b38bad709240655
Usman Nasir <usman@cyberpersons.com>
2020-06-29 21:07:02 +0500

    meta box

--- main/Providers/Hetzner.php
+++ main/Providers/Hetzner.php
@@ -15,9 +15,26 @@ class CyberPanelHetzner extends WPCPHTTP
 
     function fetchPlans(){
 
+        $this->url = 'https://api.hetzner.cloud/v1/pricing';
+
+        global $wpdb;
+
+        $result = $wpdb->get_row( "SELECT name FROM {$wpdb->prefix}cyberpanel_providers WHERE name = 'wpcp'" );
+
+        $token = json_decode($result->apidetails)->token;
+
+        $response = $this->HTTPPostCall($token);
+        $data = json_decode(wp_remote_retrieve_body($response));
+
+        $finalResult = '';
+
+        foreach ($data as $result){
+            $finalResult = $finalResult . sprintf('<option>%s</option>', $result->pricing->server_types[1]);
+        }
+
         $data = array(
             'status' => 1,
-            'result' => '<option>abc</option><option>def</option>'
+            'result' => $finalResult
         );
 
         return $data;
