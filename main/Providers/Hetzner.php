dca3c0590a3ff81e76f6d1f88433630a7b336371
Usman Nasir <usman@cyberpersons.com>
2020-07-03 19:51:07 +0500

    update pname

--- main/Providers/Hetzner.php
+++ main/Providers/Hetzner.php
@@ -84,7 +84,7 @@ class CyberPanelHetzner extends WPCPHTTP
 
         $rawHTML = '<!-- wp:html -->
 <ul class="horizontal gray">
-    <li><a href="javascript:void(0)">$productName - $serverID</a></li>
+    <li><a href="javascript:void(0)">%s</a></li>
     <li style="float:right"><a href="javascript:void(0)">Rebuild</a></li>
     <li style="float:right"><a href="javascript:void(0)">Access CyberPanel</a></li>
     <li class="rightli" style="float:right"><a href="javascript:void(0)">Manage</a></li>
@@ -142,7 +142,7 @@ class CyberPanelHetzner extends WPCPHTTP
     <!-- /wp:column --></div>
 <!-- /wp:columns -->';
 
-        $content = $rawHTML;
+        $content = sprintf($rawHTML, $productName . ' - ' . $product_id);
 
         error_log($content, 3, CPWP_ERROR_LOGS);
 
