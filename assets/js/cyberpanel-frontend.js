bfc6306df97388439cdeee58a855953203780535
Usman Nasir <usman@cyberpersons.com>
2020-07-07 01:09:46 +0500

    add modal

--- assets/js/cyberpanel-frontend.js
+++ assets/js/cyberpanel-frontend.js
@@ -25,3 +25,79 @@ function triggerModal() {
     }
 }
 
+jQuery(document).ready(function ($) {
+
+    $("#WPCPSpinner").hide();
+    $("#wpcp_providerplans_label").hide();
+    $("#wpcp_providerplans").hide();
+
+    var dataContent;
+
+    $("#connectServer").click(function () {
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'connectServer',
+            hostname: $("#hostname").val(),
+            username: $("#username").val(),
+            password: $("#password").val()
+        }
+        GlobalAjax(dataContent, verifyConnectionCB, verifyConnectionCB);
+    });
+    $("#viewJobs").click(function () {
+
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'jobStatus',
+        }
+
+        GlobalAjax(dataContent, jobStatus, jobStatus);
+
+    });
+    $("#viewJobsModal").click(function () {
+
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'jobStatus'
+        }
+
+        GlobalAjax(dataContent, jobStatus, jobStatus);
+
+    });
+
+    /// Hetzner
+
+    $("#connectProvider").click(function () {
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'connectProvider',
+            provider: $("#provider").val(),
+            name: $("#name").val(),
+            token: $("#token").val(),
+            imageID: $("#imageID").val(),
+        }
+        GlobalAjax(dataContent, verifyConnectionCB, verifyConnectionCB);
+    });
+
+    /// Providers
+
+    $("#wpcp_provider").change(function () {
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'fetchProviderPlans',
+            wpcp_provider: $(this).children("option:selected").val(),
+        }
+        GlobalAjax(dataContent, fetchProviderPlansCallBack, fetchProviderPlansCallBack);
+    });
+
+    $("#provider").change(function () {
+        dataContent = {
+            _ajax_nonce: CPWP.nonce,
+            action: 'fetchProviderAPIs',
+            provider: $(this).children("option:selected").val(),
+        }
+        GlobalAjax(dataContent, fetchProviderAPIs, fetchProviderAPIs);
+    });
+
+
+});
+
