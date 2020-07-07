cffc0e9e0b106efc642210ca7cf52ec836983e9f
Usman Nasir <usman@cyberpersons.com>
2020-07-07 09:46:29 +0500

    test jquery

--- assets/js/cyberpanel-frontend.js
+++ assets/js/cyberpanel-frontend.js
@@ -12,10 +12,6 @@ function triggerModal() {
         modal.style.display = "block";
     }
 
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
 
 // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
