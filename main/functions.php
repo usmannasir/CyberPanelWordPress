870a53f9c4df38c326b4789cf4a0364df98a9832
Usman Nasir <usman@cyberpersons.com>
2020-07-10 23:50:42 +0500

    fix garbage

--- main/functions.php
+++ main/functions.php
@@ -544,3 +544,24 @@ add_action('wpcp_croncp_hook', 'wpcp_cron_exec');
 if (!wp_next_scheduled('wpcp_croncp_hook')) {
     wp_schedule_event(time(), 'five_seconds', 'wpcp_croncp_hook');
 }
+
+///
+
+/**
+ * Display custom field on the front end
+ * @since 1.0.0
+ */
+function cfwc_display_custom_field() {
+    global $post;
+    // Check for the custom field value
+    $product = wc_get_product( $post->ID );
+    $title = $product->get_meta( 'custom_text_field_title' );
+    if( $title ) {
+        // Only display our field if we've got a value for the field title
+        printf(
+            '<div class="cfwc-custom-field-wrapper"><label for="cfwc-title-field">%s</label><input type="text" id="cfwc-title-field" name="cfwc-title-field" value=""></div>',
+            esc_html( $title )
+        );
+    }
+}
+add_action( 'woocommerce_before_add_to_cart_button', 'cfwc_display_custom_field' );
