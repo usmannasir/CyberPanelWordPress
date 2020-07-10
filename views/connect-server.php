45d48f122841e5d4d1e910f5eb3fd99f6394b1bd
Usman Nasir <usman@cyberpersons.com>
2020-07-10 12:28:17 +0500

    auto suspend/terminate

--- views/connect-server.php
+++ views/connect-server.php
@@ -8,17 +8,17 @@
         <form>
             <div class="form-group">
                 <label for="invoice" class="sr-only">Invoice</label>
-                <input type="text" id="invoice" class="form-control" placeholder="invoice" required>
-                <small class="form-text text-muted">Enter number of days before invoice is generated for servers. Default (14)</small>
+                <input type="text" id="invoice" class="form-control" placeholder="<?php get_site_option( 'wpcp_invoice', 14 , false ) ?>" required>
+                <small class="form-text text-muted">Enter number of days before invoice is generated for servers. (Default is 14)</small>
             </div>
             <div class="form-group">
                 <label for="inputEmail" class="sr-only">Auto Suspend</label>
-                <input type="text" id="autoSuspend" class="form-control" placeholder="autoSuspend" required>
+                <input type="text" id="autoSuspend" class="form-control" placeholder="placeholder="<?php get_site_option( 'wpcp_auto_suspend', 3 , false ) ?>"" required>
                 <small class="form-text text-muted">Enter number of days to suspend service after due date, if invoice is not paid (Default is 3). Enter 0 to disable auto-suspend.</small>
             </div>
             <div class="form-group">
                 <label for="inputPassword" class="sr-only">Auto Terminate</label>
-                <input type="text" id="autoTerminate" class="form-control" placeholder="Auto Terminate" required>
+                <input type="text" id="autoTerminate" class="form-control" placeholder="placeholder="<?php get_site_option( 'wpcp_terminate', 10 , false ) ?>"" required>
                 <small class="form-text text-muted">Enter number of days to terminate service after due date, if invoice is not paid (Default is 10). Enter 0 to disable auto-termination.</small>
             </div>
             <button id="connectServer" type="button" class="btn btn-primary">Save</button>
