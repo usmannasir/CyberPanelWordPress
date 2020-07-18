33b786ddb66d6baffc83957a77f2296d25c76fd8
Usman Nasir <usman@cyberpersons.com>
2020-07-18 11:52:15 +0500

    configure links in footer

--- views/footer.php
+++ views/footer.php
@@ -2,9 +2,9 @@
     <img src="https://cyberpanel.net/wp-content/uploads/2018/07/logo-e1532873145641.png"> <h4
         class="my-0 mr-md-auto font-weight-normal"><span style="margin-left: 5%">CyberPanel</span></h4>
     <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="<?php echo admin_url("admin.php?page=cyberpanel"); ?>">Add Servers</a>
+        <a class="p-2 text-dark" href="<?php echo admin_url("admin.php?page=cyberpanel"); ?>">Configure</a>
     </nav>
-    <a class="btn btn-outline-primary" href="<?php echo admin_url("admin.php?page=cyberpanel-manage"); ?>">Manage
+    <a class="btn btn-outline-primary" href="<?php echo admin_url("edit.php?post_type=wpcp_server"); ?>">Manage
         Servers</a>
     <a style="margin-left: 1%" id="viewJobs" class="btn btn-outline-info" href="#">View Jobs</a>
 </div>
