e2fb75679ac2264d857155d91a6b17c9097d44b6
Usman Nasir <usman@cyberpersons.com>
2020-07-18 13:18:54 +0500

    update help test

--- views/emails.php
+++ views/emails.php
@@ -15,29 +15,14 @@
                         <option>Server Suspended</option>
                         <option>Server Terminated</option>
                     </select>
-                    <small id="Name" class="form-text text-muted">Select cloud provider from the list.</small>
+                    <small id="Name" class="form-text text-muted">Select and configure email templates used by CyberPanel plugin.</small>
                 </div>
                 <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <label for="exampleFormControlTextarea1">Content of template</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    <small class="form-text text-muted">Give Name to this API.</small>
                 </div>
                 <button id="connectProvider" type="button" class="btn btn-primary">Save</button>
             </form>
-
-            <table style="margin-top: 2%" class="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Provider</th>
-                    <th scope="col">API Details</th>
-                    <th scope="col">Actions</th>
-                </tr>
-                </thead>
-                <tbody id="wpcp_providerapis">
-                </tbody>
-            </table>
         </div>
     </div>
 
