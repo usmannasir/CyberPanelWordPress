80151d08078ff3cc4d9a43560cd5558432660e28
Usman Nasir <usman@cyberpersons.com>
2020-07-19 14:13:39 +0500

    allow to save manual server state

--- main/functions.php
+++ main/functions.php
@@ -463,7 +463,7 @@ function wpcp_custom_post_type()
             'public' => true,
             'has_archive' => false,
             'delete_with_user' => false,
-            "supports" => array(),
+            "supports" => array("customer"),
             //'capability_type' => 'product'
         )
     );
